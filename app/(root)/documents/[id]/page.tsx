cfde44cfedfd6d55f230eda02cb4bcc2a3036033
orzarhi <or.zarhi11@gmail.com>
2024-07-30 01:01:11 +0300

    feat: Add header component and update document page
    This commit adds a new "Header" component to the project, which is used in the "Document" page. The "Header" component includes a logo and a link to the homepage. The "Document" page is updated to include the "Header" component and display a fake document title. This improves the overall layout and navigation of the application.

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { Editor } from "@/components/editor/Editor";
+import { Header } from "@/components/Header";
+import React from "react";
 
 export default function Document() {
   return (
-    <div>page</div>
-  )
+    <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">this is a fake document title</p>
+        </div>
+      </Header>
+      <Editor />
+    </div>
+  );
 }
