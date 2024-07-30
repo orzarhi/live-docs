e9e15db8833e7702564d3bcb40a5a40a96c91ae4
orzarhi <or.zarhi11@gmail.com>
2024-07-30 22:45:45 +0300

    feat: Add lazy loading for images and update layout

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,14 +1,14 @@
 // Define Liveblocks types for your application
-// https://liveblocks.io/docs/api-reference/liveblocks-client#Typing-your-data
+// https://liveblocks.io/docs/api-reference/liveblocks-react#Typing-your-data
 declare global {
   interface Liveblocks {
-    // Each user's Presence, for room.getPresence, room.subscribe("others"), etc.
+    // Each user's Presence, for useMyPresence, useOthers, etc.
     Presence: {
       // Example, real-time cursor coordinates
       // cursor: { x: number; y: number };
     };
 
-    // The Storage tree for the room, for room.getStorage, room.subscribe(storageItem), etc.
+    // The Storage tree for the room, for useMutation, useStorage, etc.
     Storage: {
       // Example, a conflict-free list
       // animals: LiveList<string>;
@@ -18,28 +18,26 @@ declare global {
     UserMeta: {
       id: string;
       info: {
-        id: string;
-        name: string;
-        email: string;
-        avatar: string;
-        color: string;
+        // Example properties, for useSelf, useUser, useOthers, etc.
+        // name: string;
+        // avatar: string;
       };
     };
 
-    // Custom events, for room.broadcastEvent, room.subscribe("event")
+    // Custom events, for useBroadcastEvent, useEventListener
     RoomEvent: {};
-    // Example has two events, using a union
-    // | { type: "PLAY" }
-    // | { type: "REACTION"; emoji: "🔥" };
+      // Example has two events, using a union
+      // | { type: "PLAY" } 
+      // | { type: "REACTION"; emoji: "🔥" };
 
-    // Custom metadata set on threads, for use in React
+    // Custom metadata set on threads, for useThreads, useCreateThread, etc.
     ThreadMetadata: {
       // Example, attaching coordinates to a thread
       // x: number;
       // y: number;
     };
 
-    // Custom room info set with resolveRoomsInfo, for use in React
+    // Custom room info set with resolveRoomsInfo, for useRoomInfo
     RoomInfo: {
       // Example, rooms with a title and url
       // title: string;
